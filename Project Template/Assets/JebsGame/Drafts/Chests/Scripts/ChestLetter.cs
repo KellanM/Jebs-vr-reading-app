e18d1802895bf09baf755cbd5c62d12969141be2
JebsGame <JebsReadingGame@gmail.com>
2020-06-26 11:39:03 +0200

    Chests gamemode draft

---
+++ Project Template/Assets/JebsGame/Drafts/Chests/Scripts/ChestLetter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ChestLetter : MonoBehaviour
+{
+    public char value;
+
+    public XRBaseInteractor currentInteractor;
+    XRBaseInteractor previousInteractor;
+
+    XRGrabInteractable interactable;
+
+    private void Start()
+    {
+        interactable = GetComponent<XRGrabInteractable>();
+        interactable.onSelectEnter.AddListener(SetGrabbed);
+        interactable.onSelectExit.AddListener(SetUngrabbed);
+    }
+
+    public void SetGrabbed(XRBaseInteractor interactor)
+    {
+        currentInteractor = interactor;
+    }
+
+    public void SetUngrabbed(XRBaseInteractor interactor)
+    {
+        currentInteractor = null;
+    }
+}
