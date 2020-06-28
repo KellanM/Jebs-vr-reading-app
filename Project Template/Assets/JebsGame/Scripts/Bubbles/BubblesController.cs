f5408a06ef67b13570117769fa27819ef8a68f9d
X0FloH <33568643+X0FloH@users.noreply.github.com>
2020-06-28 15:33:36 +0100

    Added base gun handling and gun returns to base plate

---
+++ Project Template/Assets/JebsGame/Scripts/Bubbles/BubblesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubblesController : MonoBehaviour
+{
+    [Header("References")]
+    public GameObject bubbleGun;
+    public Transform gunRestPlace;
+
+    private void Update()
+    {
+        //If gun is not held, lerp to gunRestPlace
+        if(bubbleGun.GetComponent<GrabIsHeld>().isHeld == false)
+        {
+            bubbleGun.GetComponent<Rigidbody>().velocity /= 2;
+            bubbleGun.GetComponent<Rigidbody>().angularVelocity /= 2;
+            bubbleGun.transform.position = Vector3.Lerp(bubbleGun.transform.position, gunRestPlace.transform.position, 3f * Time.deltaTime);
+            bubbleGun.transform.rotation = Quaternion.Lerp(bubbleGun.transform.rotation, gunRestPlace.transform.rotation, 3f * Time.deltaTime);
+        }
+    }
+
+}
