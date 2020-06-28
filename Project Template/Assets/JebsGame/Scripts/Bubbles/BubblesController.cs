2c83419c7470fe388974186ce7f25820eb427c5b
X0FloH <33568643+X0FloH@users.noreply.github.com>
2020-06-28 21:49:48 +0100

    Got the bubble matrix working

--- Project Template/Assets/JebsGame/Scripts/Bubbles/BubblesController.cs
+++ Project Template/Assets/JebsGame/Scripts/Bubbles/BubblesController.cs
@@ -1,12 +1,107 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+public class ListTools
+{
+    public static T RandomValueIn<T> (List<T> list)
+    {
+        int chosen = Random.Range(0, list.Count);
+        return list[chosen];
+    }
+}
 
 public class BubblesController : MonoBehaviour
 {
     [Header("References")]
     public GameObject bubbleGun;
     public Transform gunRestPlace;
+    public GameObject bubbleEmitter;
+
+    public List<GameObject> letterBubblePrefabs = new List<GameObject>();
+
+    [Header("Script Data")]
+    public List<GameObject> instantiatedBubbles = new List<GameObject>();
+    [SerializeField] public List<GameObject> instantiateQueue = new List<GameObject>();
+
+    public List<string> shotBubbles = new List<string>();
+
+    [Header("Settings")]
+    public int gridSize;
+
+    public float baseY;
+
+    public float xBetween;
+    public float yBetween;
+
+    IEnumerator SpawnBubbles()
+    {
+        yield return new WaitForSeconds(0.2f);
+        instantiateQueue = new List<GameObject>(gridSize * gridSize);
+
+        //Populate the list with nulls
+        for(int i = 0; i < gridSize * gridSize; i++)
+        {
+            instantiateQueue.Add(null);
+        }
+
+        //Make sure atleast one of each letter is queued to spawn;
+        foreach(GameObject letterBubblePrefab in letterBubblePrefabs)
+        {
+            //Make sure doesnt override other letter queued
+            bool queued = false;
+            while (!queued)
+            {
+                int chosenId = Random.Range(0, gridSize * gridSize);
+                if(instantiateQueue[chosenId] == null)
+                {
+                    instantiateQueue[chosenId] = letterBubblePrefab;
+                    queued = true;
+                }
+            }
+        }
+
+        //Populate unpopulated places in the grid
+        for(int i = 0; i < gridSize * gridSize; i += 1)
+        {
+            if(instantiateQueue[i] == null)
+            {
+                instantiateQueue[i] = ListTools.RandomValueIn(letterBubblePrefabs);
+            }
+        }
+
+        //Instantiate all queued Bubbles
+        for(int i = 0; i < instantiateQueue.Count; i += 1)
+        {
+            GameObject newBubble = Instantiate(instantiateQueue[i], bubbleEmitter.transform.position, Quaternion.identity, null);
+            instantiatedBubbles.Add(newBubble);
+
+            newBubble.transform.name = instantiateQueue[i].name;
+
+            //Calculate target position
+            float x = (bubbleEmitter.transform.position.x - ((xBetween * gridSize) / 2)) + (xBetween * (i - (gridSize * (Mathf.Floor((i)/gridSize))))) + xBetween/2;
+            float y = baseY + ((yBetween * gridSize) - ((Mathf.Floor((i) / gridSize)) * yBetween));
+
+            newBubble.AddComponent<LetterDisplayLerp>().toPos = new Vector3(x, y, newBubble.transform.position.z);
+            yield return new WaitForSeconds(0.02f);
+        }
+
+    }
+
+    public void ShotBubble(GameObject bubble)
+    {
+        //Called by gun when bubble shot
+        shotBubbles.Add(bubble.transform.name);
+        bubble.transform.GetChild(0).gameObject.AddComponent<Rigidbody>();
+        bubble.transform.GetChild(0).parent = null;
+        bubble.SetActive(false);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SpawnBubbles());
+    }
 
     private void Update()
     {
---
+++ Project Template/Assets/JebsGame/Scripts/Bubbles/BubblesShoot.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubblesShoot : MonoBehaviour
+{
+    [Header("References")]
+    public BubblesController controller;
+
+    public Transform shootPlace;
+
+    [Header("Settings")]
+    public float shootThreshold;
+    public float shootTimeout;
+
+
+    bool timedout;
+    bool shot;
+
+    private void Update()
+    {
+    }
+
+    void AllowShot()
+    {
+        timedout = false;
+    }
+
+    public void TriggerDown()
+    {
+        if (!timedout && !shot)
+        {
+            Debug.DrawRay(shootPlace.transform.position, shootPlace.transform.forward * 500f, Color.red, 5f);
+            RaycastHit hit;
+            if (Physics.Raycast(shootPlace.transform.position, shootPlace.transform.forward, out hit))
+            {
+                if (hit.transform.root.gameObject.tag == "Bubble")
+                {
+                    controller.ShotBubble(hit.transform.root.gameObject);
+                }
+            }
+            timedout = true;
+            shot = true;
+            CancelInvoke();
+            Invoke("AllowShot", shootTimeout);
+        }
+    }
+
+    public void TriggerUp()
+    {
+        shot = false;
+    }
+}
