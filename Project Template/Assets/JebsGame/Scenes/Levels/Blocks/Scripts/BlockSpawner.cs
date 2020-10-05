95a943da107df2b36c14592728b49df6d77c255b
Oxters <oxters168@gmail.com>
2020-10-06 02:09:58 +0300

    Created a block spawner object in the blocks mode that is triggered by the button. Attempting to fix some issues with the stacking of blocks in blocks mode.

---
+++ Project Template/Assets/JebsGame/Scenes/Levels/Blocks/Scripts/BlockSpawner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityHelpers;
+
+public class BlockSpawner : MonoBehaviour
+{
+    public Transform blockPrefab;
+    private ObjectPool<Transform> BlocksPool { get { if (_blocksPool == null) _blocksPool = new ObjectPool<Transform>(blockPrefab, 5, false, true); return _blocksPool; } }
+    private ObjectPool<Transform> _blocksPool;
+
+    public void SpawnCube()
+    {
+        BlocksPool.Get((block) => { block.position = transform.position; });
+    }
+}
--- Project Template/Assets/JebsGame/Scenes/Levels/Blocks/Scripts/StackStability.cs
+++ Project Template/Assets/JebsGame/Scenes/Levels/Blocks/Scripts/StackStability.cs
@@ -8,9 +8,11 @@ public class StackStability : MonoBehaviour
     private PhysicsTransform PhysicsSelf { get { if (_physicsSelf == null) _physicsSelf = GetComponent<PhysicsTransform>(); return _physicsSelf; } }
     private PhysicsTransform _physicsSelf;
 
+    public float groundDistDetection = 0.001f;
     public bool isGrounded;
     public bool isStacked;
     private bool _prevStacked;
+    private StackStability undercube;
 
     void Update()
     {
@@ -22,13 +24,14 @@ public class StackStability : MonoBehaviour
     {
         var bounds = transform.GetTotalBounds(Space.World);
         RaycastHit raycastHit;
-        Debug.DrawRay(transform.position, Vector3.down * (bounds.extents.y + 0.01f), Color.green);
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, out raycastHit, bounds.extents.y + 0.01f);
+        Debug.DrawRay(transform.position, Vector3.down * (bounds.extents.y + groundDistDetection), Color.green);
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, out raycastHit, bounds.extents.y + groundDistDetection);
         if (isGrounded)
         {
             var otherCube = raycastHit.transform.GetComponentInParent<StackStability>();
             if (otherCube != null && otherCube.isGrounded)
             {
+                undercube = otherCube;
                 isStacked = GrabbableSelf.GetGrabCount() <= 0;
             }
             else
@@ -37,15 +40,23 @@ public class StackStability : MonoBehaviour
         else
             isStacked = false;
     }
+
     private void AnchorToStack()
     {
         if (isStacked)
         {
             if (!_prevStacked)
             {
-                PhysicsSelf.position = transform.position;
-                PhysicsSelf.rotation = transform.rotation;
-                _prevStacked = true;
+                var cubeBounds = undercube.transform.GetTotalBounds(Space.Self);
+                Vector3 upAlignedAxis = GetAxisAlignedTo(undercube.transform, Vector3.up);
+                Vector3 stackedPosition = undercube.transform.position + upAlignedAxis * cubeBounds.size.y;
+
+                Vector3 selfUpAligned = GetAxisAlignedTo(transform, Vector3.up);
+                Vector3 selfForwardAligned = GetAxisAlignedTo(transform, Vector3.forward);
+                Quaternion stackedOrientation = Quaternion.LookRotation(selfForwardAligned, selfUpAligned);
+
+                PhysicsSelf.position = stackedPosition;
+                PhysicsSelf.rotation = stackedOrientation.Shorten();
             }
 
             PhysicsSelf.striveForPosition = true;
@@ -58,8 +69,30 @@ public class StackStability : MonoBehaviour
                 PhysicsSelf.striveForPosition = false;
                 PhysicsSelf.striveForOrientation = false;
             }
-
-            _prevStacked = false;
         }
+
+        _prevStacked = isStacked;
+    }
+
+    public static Vector3 GetAxisAlignedTo(Transform orientedObject, Vector3 worldDirection)
+    {
+        Vector3 upAlignedAxis = orientedObject.up;
+        float closestDot = Vector3.Dot(upAlignedAxis, Vector3.up);
+        float otherDot = Vector3.Dot(orientedObject.right, Vector3.up);
+        if (Mathf.Abs(otherDot) > Mathf.Abs(closestDot))
+        {
+            upAlignedAxis = orientedObject.right;
+            closestDot = otherDot;
+        }
+        otherDot = Vector3.Dot(orientedObject.forward, Vector3.up);
+        if (Mathf.Abs(otherDot) > Mathf.Abs(closestDot))
+        {
+            upAlignedAxis = orientedObject.forward;
+            closestDot = otherDot;
+        }
+        if (otherDot < 0)
+            upAlignedAxis *= -1;
+
+        return upAlignedAxis;
     }
 }
